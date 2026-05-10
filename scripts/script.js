e336af73684b0dae15a595d076b054ab87805f5f
BHEnergy <sabaevmihail753@gmail.com>
2026-05-11 00:10:00 +0300

    Предварительная версия Главной

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    let buttonsReset = Array.from(document.querySelectorAll('button'));
+    buttonsReset.forEach( (el) => {
+        el.addEventListener('click', (event) => {
+            event.preventDefault();
+        })
+    })
+
     const firstBanner = new Swiper('.js-init-first__banner', {
         slidesPerView: 1,
         loop: true,
@@ -7,9 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
             delay: 10000
         },
         pagination: {
-            el: '.swiper-pagination',
+            el: '.first__banner-pagination',
             type: 'bullets',
             bulletClass: 'pagination-bullet__bar'
         }
-    })
+    });
+
+    const reviews = new Swiper('.js-init-review-slider', {
+        slidesPerView: 1,
+        loop: true,
+        speed: 3000,
+        spaceBetween: 40,
+        autoplay: {
+            delay: 10000
+        },
+        pagination: {
+            el: '.reviews-pagination',
+            type: 'bullets',
+            bulletClass: 'pagination-bullet__bar'
+        }
+    });
 })
